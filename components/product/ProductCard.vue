b9a35fa77e940712f8d7f8445d142970d56c109b
Alex Asche <alexcasche@gmail.com>
2022-01-27 11:15:54 -0500

    chore: sections wired

---
+++ components/product/ProductCard.vue
@@ -0,0 +1,127 @@
+<template>
+  <div @mouseenter="handleHover(true)" @mouseleave="handleHover(false)">
+    <div
+      class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8"
+    >
+      <nuxt-link :to="`/products/${product.content.handle}`" class="group">
+        <nuxt-picture
+          :src="product.content.featuredMedia.src"
+          :alt="product.content.featuredMedia.altText"
+          sizes="sm:100vw lg:50vw xl:25vw"
+          quality="80"
+          class="picture"
+        />
+      </nuxt-link>
+    </div>
+    <div v-show="isHovered">
+      <div v-if="product.variants.length > 1">
+        <!-- <select
+          class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md mt-3"
+          @change="selectVariant($event)"
+        >
+          <option
+            v-for="variant in product.variants"
+            :key="variant.id"
+            :value="variant.id"
+          >
+            {{ variant.title }}
+          </option>
+        </select>
+        <button
+          type="button"
+          :disabled="!selectedVariant.availableForSale"
+          class="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200 w-full mt-3"
+          @click="addProduct(product)"
+        >
+          <div v-if="selectedVariant.availableForSale">Add to Cart</div>
+          <div v-else>Out of Stock</div>
+        </button> -->
+      </div>
+      <div v-else class="flex w-full">
+        <!-- <button
+          type="button"
+          :disabled="!selectedVariant.availableForSale"
+          class="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200 w-full mt-5"
+          @click="addProduct(product)"
+        >
+          <div v-if="selectedVariant.availableForSale">Add to Cart</div>
+          <div v-else>Out of Stock</div>
+        </button> -->
+      </div>
+    </div>
+    <div v-show="!isHovered">
+      <h3 class="mt-4 text-sm text-gray-700">
+        {{ product.title }}
+      </h3>
+      <p v-if="price" class="mt-1 text-lg font-medium text-gray-900">
+        {{ price }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { formatPrice } from '~/utils/formatPrice';
+// import Price from "~/components/core/Price.vue";
+
+export default {
+  name: 'ProductCard',
+  components: {
+    // Price
+  },
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({
+    isHovered: false
+  }),
+  computed: {
+    price() {
+      return formatPrice({ price: this.product.variants[0].price });
+    }
+  },
+  methods: {
+    handleHover(value) {
+      this.isHovered = value;
+    }
+  }
+  // setup(props) {
+  //   const { addItem } = useCartProvider();
+  //   const setCartOpen = inject("setCartOpen");
+  //   const selectedVariant = ref(props.product.variants[0]);
+  //   const selectVariant = event => {
+  //     selectedVariant.value = props.product.variants.find(
+  //       variant => variant.id === event.target.value
+  //     );
+  //   };
+  //   const addProduct = product => {
+  //     setCartOpen(true);
+  //     addItem({
+  //       product,
+  //       variant: selectedVariant.value,
+  //       quantity: 1
+  //     });
+  //   };
+  //   const hover = ref(false);
+  //   const setHover = bool => {
+  //     hover.value = bool;
+  //   };
+  //   return {
+  //     hover,
+  //     setHover,
+  //     addProduct,
+  //     selectVariant,
+  //     selectedVariant
+  //   };
+  // }
+};
+</script>
+
+<style lang="scss" scoped>
+::v-deep .picture img {
+  @apply w-full h-full object-center object-cover group-hover:opacity-75;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="homePage">Homepage</div>
+  <div class="homePage">
+    <site-section
+      v-for="section in page.fields.sections"
+      :key="section._key"
+      :content="section"
+    />
+  </div>
 </template>
 
 <script>
+import { CONTENT_PAGE_QUERY } from '~/queries/contentPage';
+import SiteSection from '~/components/section/Section.vue';
+
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: { SiteSection },
+  async asyncData({ app }) {
+    const { pages } = await app.$nacelle.query({
+      query: CONTENT_PAGE_QUERY,
+      variables: { handle: 'page-homepage' }
+    });
+    return {
+      page: pages[0]
+    };
+  }
 };
 </script>
 
