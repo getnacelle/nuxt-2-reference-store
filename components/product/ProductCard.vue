3d336fbcbc1ee9cd25d19bf08e952b0eaf99f502
Alex Asche <alexcasche@gmail.com>
2022-01-27 11:33:34 -0500

    chore: product card done

--- components/product/ProductCard.vue
+++ components/product/ProductCard.vue
@@ -14,44 +14,33 @@
       </nuxt-link>
     </div>
     <div v-show="isHovered">
-      <div v-if="product.variants.length > 1">
-        <!-- <select
+      <div v-for="option in options" :key="option.name">
+        <select
           class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md mt-3"
-          @change="selectVariant($event)"
+          @change="($event) => handleOptionChange($event, option)"
         >
           <option
-            v-for="variant in product.variants"
-            :key="variant.id"
-            :value="variant.id"
+            v-for="(value, vIndex) in option.values"
+            :key="vIndex"
+            :value="value"
           >
-            {{ variant.title }}
+            {{ value }}
           </option>
         </select>
-        <button
-          type="button"
-          :disabled="!selectedVariant.availableForSale"
-          class="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200 w-full mt-3"
-          @click="addProduct(product)"
-        >
-          <div v-if="selectedVariant.availableForSale">Add to Cart</div>
-          <div v-else>Out of Stock</div>
-        </button> -->
       </div>
-      <div v-else class="flex w-full">
-        <!-- <button
-          type="button"
-          :disabled="!selectedVariant.availableForSale"
-          class="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200 w-full mt-5"
-          @click="addProduct(product)"
-        >
-          <div v-if="selectedVariant.availableForSale">Add to Cart</div>
-          <div v-else>Out of Stock</div>
-        </button> -->
-      </div>
+      <button
+        type="button"
+        :disabled="!selectedVariant.availableForSale"
+        class="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200 w-full mt-3"
+        @click="handleAddItem"
+      >
+        <div v-if="selectedVariant.availableForSale">Add to Cart</div>
+        <div v-else>Out of Stock</div>
+      </button>
     </div>
     <div v-show="!isHovered">
       <h3 class="mt-4 text-sm text-gray-700">
-        {{ product.title }}
+        {{ product.content.title }}
       </h3>
       <p v-if="price" class="mt-1 text-lg font-medium text-gray-900">
         {{ price }}
@@ -61,14 +50,14 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 import { formatPrice } from '~/utils/formatPrice';
-// import Price from "~/components/core/Price.vue";
+import { getSelectedVariant } from '~/utils/getSelectedVariant';
+import { getCartVariant } from '~/utils/getCartVariant';
 
 export default {
   name: 'ProductCard',
-  components: {
-    // Price
-  },
   props: {
     product: {
       type: Object,
@@ -76,47 +65,54 @@ export default {
     }
   },
   data: () => ({
+    selectedVariant: null,
+    selectedOptions: null,
     isHovered: false
   }),
   computed: {
+    options() {
+      const optionsExist = this.product?.content?.options?.find((option) => {
+        return option.values.length > 1;
+      });
+      return optionsExist ? this.product?.content?.options : null;
+    },
     price() {
       return formatPrice({ price: this.product.variants[0].price });
     }
   },
+  created() {
+    this.selectedVariant = { ...this.product.variants[0] };
+    this.selectedOptions = [...this.selectedVariant.content.selectedOptions];
+  },
   methods: {
+    ...mapMutations('cart', ['addItem']),
     handleHover(value) {
       this.isHovered = value;
+    },
+    handleOptionChange($event, option) {
+      const newOption = { name: option.name, value: $event.target.value };
+      const optionIndex = this.selectedOptions.findIndex((selectedOption) => {
+        return selectedOption.name === newOption.name;
+      });
+      if (optionIndex > -1)
+        this.selectedOptions.splice(optionIndex, 1, newOption);
+      else this.selectedOptions.push(newOption);
+      const variant = getSelectedVariant({
+        product: this.product,
+        options: this.selectedOptions
+      });
+      this.selectedVariant = variant ? { ...variant } : null;
+    },
+    handleAddItem() {
+      this.addItem({
+        ...getCartVariant({
+          product: this.product,
+          variant: this.selectedVariant
+        }),
+        quantity: 1
+      });
     }
   }
-  // setup(props) {
-  //   const { addItem } = useCartProvider();
-  //   const setCartOpen = inject("setCartOpen");
-  //   const selectedVariant = ref(props.product.variants[0]);
-  //   const selectVariant = event => {
-  //     selectedVariant.value = props.product.variants.find(
-  //       variant => variant.id === event.target.value
-  //     );
-  //   };
-  //   const addProduct = product => {
-  //     setCartOpen(true);
-  //     addItem({
-  //       product,
-  //       variant: selectedVariant.value,
-  //       quantity: 1
-  //     });
-  //   };
-  //   const hover = ref(false);
-  //   const setHover = bool => {
-  //     hover.value = bool;
-  //   };
-  //   return {
-  //     hover,
-  //     setHover,
-  //     addProduct,
-  //     selectVariant,
-  //     selectedVariant
-  //   };
-  // }
 };
 </script>
 
